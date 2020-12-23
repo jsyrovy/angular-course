dc779125a3b282e775457432c91b4aa28dbe8cea
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-23 12:05:51 +0100

    Add string interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+}
