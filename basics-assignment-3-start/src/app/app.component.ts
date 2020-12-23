c3f881c65f58f9bee6e657fb0af470404dd8dc7e
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-23 17:12:06 +0100

    Add functionality

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  showDetails = false;
+  history = [];
+  currentId = 0;
+
+  onShowDetails() {
+    this.showDetails = !this.showDetails;
+    this.history.push({ id: this.currentId, status: this.showDetails });
+    this.currentId++;
+  }
 }
