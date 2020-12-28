c66d6cef65f84c03382422cfbeb72224696a44b4
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-28 15:42:43 +0100

    Add functionality

--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./game-control.component.css"],
 })
 export class GameControlComponent implements OnInit {
+  cycle = 0;
+  interval;
+  oddNumbers: Array<number> = [];
+  evenNumbers: Array<number> = [];
   constructor() {}
 
   ngOnInit(): void {}
+
+  onStart() {
+    this.interval = setInterval(this.doCycle.bind(this), 1000);
+  }
+
+  doCycle() {
+    this.cycle++;
+
+    if (this.isEven(this.cycle)) {
+      this.evenNumbers.push(this.cycle);
+      return;
+    }
+    this.oddNumbers.push(this.cycle);
+
+    console.log(this.cycle);
+  }
+
+  isEven(n: number) {
+    return n % 2 == 0;
+  }
+
+  onStop() {
+    clearInterval(this.interval);
+  }
 }
