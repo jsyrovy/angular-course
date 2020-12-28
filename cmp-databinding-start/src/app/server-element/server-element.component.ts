a3ed8a828174a154b4a31a53bd95fbbc628e84b3
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-28 14:07:50 +0100

    Refactor code

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,11 +6,7 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
 })
 export class ServerElementComponent implements OnInit {
-  @Input("serverElement") element: {
-    type: string;
-    name: string;
-    content: string;
-  };
+  @Input() name: string;
 
   constructor() {}
 
