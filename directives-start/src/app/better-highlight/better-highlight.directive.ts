0f0b7fb561a22d5fa8b03ad090ead3578dee585a
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-29 10:18:23 +0100

    Replace HostListener with HostBinding

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -4,29 +4,24 @@ import {
   Renderer2,
   ElementRef,
   HostListener,
+  HostBinding,
 } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]",
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {}
 
   @HostListener("mouseenter") mouseEnter(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "backgroundColor",
-      "blue"
-    );
+    this.backgroundColor = "blue";
   }
 
   @HostListener("mouseleave") mouseLeave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "backgroundColor",
-      "transparent"
-    );
+    this.backgroundColor = "transparent";
   }
 }
