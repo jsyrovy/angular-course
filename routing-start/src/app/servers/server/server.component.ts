f0aad00f2cda8b925f7e71557501bd950605621a
Jiri Syrovy <jiri.syrovy@gmail.com>
2020-12-31 14:20:48 +0100

    Add dynamic data resolving

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
+import { ActivatedRoute, Data, Router } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -19,13 +19,8 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(
-      +this.activatedRoute.snapshot.params["id"]
-    );
-    this.activatedRoute.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(
-        +this.activatedRoute.snapshot.params["id"]
-      );
+    this.route.data.subscribe((data: Data) => {
+      this.server = data["server"];
     });
   }
 
