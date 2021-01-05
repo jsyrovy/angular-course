2e1e9c850cd5d83a959297ec42d6ea2c6c7db804
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-05 17:49:45 +0100

    Add form data usage

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer = "";
   genders = ["male", "female", "other"];
+  submitted = false;
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
 
   suggestUserName() {
     const suggestedName = "Superuser";
@@ -19,5 +27,13 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.form);
+
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    this.submitted = true;
   }
 }
