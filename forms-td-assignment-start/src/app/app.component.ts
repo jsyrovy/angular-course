2f26a28a9e69cfb441aa1fce19bd8f0cbd6c0616
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-05 18:18:30 +0100

    Add form

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  defaultSubscription = "Advanced";
+  @ViewChild("f") form: NgForm;
+  account = { email: "", subscription: "", password: "" };
 
+  onSubmit() {
+    console.log(this.form);
+
+    this.account.email = this.form.value.email;
+    this.account.subscription = this.form.value.subscription;
+    this.account.password = this.form.value.password;
+
+    this.form.reset();
+  }
 }
