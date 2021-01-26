00c8d2972853917b5c11e46b84fcb4c7eb201b6c
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-26 14:04:11 +0100

    Solve assignment

--- up-and-running-with-jquery/assignment_C/js/custom_jquery_code.js
+++ up-and-running-with-jquery/assignment_C/js/custom_jquery_code.js
@@ -1,3 +1,7 @@
 $("button").on("click", function () {
-  // Fill In your Code here
+  const qty = parseFloat($("#quant_1").val());
+  const rate = parseFloat($("#rate_1").val());
+  const amount = qty * rate;
+  $("#amount_1").show().val(amount);
+  $("#output").text("Amount Charged for quantity of " + qty + " is $" + amount);
 });
