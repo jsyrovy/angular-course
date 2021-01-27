69c01bf976a7b8d9db634160f3e9dd57f1acb6dd
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-27 10:25:42 +0100

    Add dom manipulation methods

--- up-and-running-with-jquery/jquery_tutorial_12/js/custom_jquery_code.js
+++ up-and-running-with-jquery/jquery_tutorial_12/js/custom_jquery_code.js
@@ -1,3 +1,10 @@
 console.log($("#courses").find("li"));
 console.log($("#courses").find("li").children());
 console.log($("#courses").find("li").children().children());
+
+const maths = $("#math_subjects");
+const newEl1 = $("<li>New Element 1</li>");
+const newEl2 = $("<li>New Element 2</li>");
+
+maths.prepend(newEl1);
+maths.append(newEl2);
