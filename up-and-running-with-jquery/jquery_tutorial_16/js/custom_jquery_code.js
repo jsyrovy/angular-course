98447b118ee4e8b46a7dd39259d380d85b1d441c
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-27 13:00:08 +0100

    Add ajax requests

--- up-and-running-with-jquery/jquery_tutorial_16/js/custom_jquery_code.js
+++ up-and-running-with-jquery/jquery_tutorial_16/js/custom_jquery_code.js
@@ -1,7 +1,55 @@
-// You'll need the below URLs while following along with the lecture for practice.
-// var root = "https://jsonplaceholder.typicode.com/posts/1";
-// var root = "https://jsonplaceholder.typicode.com/comments";
-// var url = "https://raw.githubusercontent.com/imtiazahmad007/resources/master/some_random_text.txt";
-// var url = "https://raw.githubusercontent.com/imtiazahmad007/resources/master/sample_table.html"
+$(function () {
+  $("#btn_1").on("click", function () {
+    // $("div.main").load(
+    //   "https://raw.githubusercontent.com/imtiazahmad007/resources/master/some_random_text.txt"
+    // );
+    // $("div.main").load(
+    //   "https://raw.githubusercontent.com/imtiazahmad007/resources/master/sample_table.html"
+    // );
+    // $("div.main").load(
+    //   "https://raw.githubusercontent.com/imtiazahmad007/resources/master/sample_table.html table fieldset:first"
+    // );
 
-$(function () {});
+    // $.ajax(
+    //   "https://raw.githubusercontent.com/imtiazahmad007/resources/master/sample_table.html",
+    //   {
+    //     dataType: "html",
+    //     method: "GET",
+    //     success: function (response) {
+    //       $("div.main").html(response);
+    //     },
+    //     error: function (request, errorType, errorMsg) {
+    //       console.log("errorMsg", errorMsg);
+    //     },
+    //   }
+    // );
+
+    // $.ajax("https://jsonplaceholder.typicode.com/posts/1", {
+    //   dataType: "json",
+    //   method: "GET",
+    //   success: function (response) {
+    //     console.log(response);
+    //     $("div.main").append($("<p>").text("User ID: " + response.userId));
+    //   },
+    //   error: function (request, errorType, errorMsg) {
+    //     console.log("errorMsg", errorMsg);
+    //   },
+    // });
+
+    $.ajax("https://jsonplaceholder.typicode.com/comments", {
+      dataType: "json",
+      method: "GET",
+      data: { postId: 1 },
+      success: function (response) {
+        console.log(response);
+
+        response.forEach((comment) => {
+          $("div.main").append($("<p>").text("Name: " + comment.name));
+        });
+      },
+      error: function (request, errorType, errorMsg) {
+        console.log("errorMsg", errorMsg);
+      },
+    });
+  });
+});
