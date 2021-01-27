7375b6a440bac30aa416e9de95c9e66775a16c5e
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-27 11:20:28 +0100

    Solve assignment

--- up-and-running-with-jquery/assignment_E/js/custom_jquery_code.js
+++ up-and-running-with-jquery/assignment_E/js/custom_jquery_code.js
@@ -1,1 +1,20 @@
-$(function () {});
+$(function () {
+  $("#btn_1").on("click", function () {
+    const name = $("#course_name").children("input").first().val();
+    const statement = $("#statement").children("h3").first();
+
+    statement.text(name);
+    statement.css({ "background-color": "green", color: "white" });
+  });
+
+  $("li").on("click", function (event) {
+    const name = $("#course_name").children("input").first().val();
+    const el = $("<li>").text(name);
+
+    el.css("background-color", "yellow");
+
+    $(this).append(el);
+
+    event.stopPropagation();
+  });
+});
