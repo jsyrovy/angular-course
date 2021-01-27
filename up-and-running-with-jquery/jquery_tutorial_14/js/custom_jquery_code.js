c0a6a71af2f150ed60876f6cabae089c5223d312
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-27 11:00:47 +0100

    Add events

--- up-and-running-with-jquery/jquery_tutorial_14/js/custom_jquery_code.js
+++ up-and-running-with-jquery/jquery_tutorial_14/js/custom_jquery_code.js
@@ -1,1 +1,33 @@
-$(function () {});
+$(function () {
+  $("button").on("click", function (event) {
+    const button = $(this);
+    const parent = button.parent();
+    const author = parent.data("author");
+    const price = parent.data("price");
+    const purchased = parent.data("purchased");
+    const myRating = parent.data("my-rating");
+
+    parent.css("background-color", "yellow");
+    parent.append($("<p>Author: " + author + "</p>"));
+
+    if (price !== undefined) {
+      parent.append($("<p>Price: $" + price + "</p>"));
+    }
+
+    if (purchased !== undefined) {
+      parent.append($("<p>Purchased: " + purchased + "</p>"));
+    }
+
+    if (myRating !== undefined) {
+      parent.append($("<p>My Rating: " + myRating + "</p>"));
+    }
+
+    button.remove();
+
+    event.stopPropagation();
+  });
+
+  $("div.book").on("click", function () {
+    alert("Click on the see details button to see the price of the book.");
+  });
+});
