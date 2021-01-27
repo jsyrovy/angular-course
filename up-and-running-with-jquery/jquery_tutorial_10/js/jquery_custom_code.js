30eb86ead093ea98ae8345219f982a4c9e765582
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-01-27 10:01:25 +0100

    Add selectors

--- up-and-running-with-jquery/jquery_tutorial_10/js/jquery_custom_code.js
+++ up-and-running-with-jquery/jquery_tutorial_10/js/jquery_custom_code.js
@@ -1,1 +1,6 @@
-$(document).ready(function () {});
+$(function () {
+  $("#steps > li").css("color", "blue");
+  $("#steps li:first,#nested_steps li:first").css("color", "red");
+  $("#steps li:nth-child(3)").css("border", "1px solid orange");
+  $("p[class]").css("color", "purple");
+});
