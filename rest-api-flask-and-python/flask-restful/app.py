441afbab87592b2f7083cbe9a35aad1e459a96e5
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-05-24 15:01:12 +0200

    Add sqlalchemy

--- rest-api-flask-and-python/flask-restful/app.py
+++ rest-api-flask-and-python/flask-restful/app.py
@@ -2,20 +2,33 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from resources.user import UserRegister
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "yes, it's bad"
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    db.init_app(app)
+    app.run(port=5000, debug=True)
